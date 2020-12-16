f6f385f69d166ff717cf531fe3243bc21136346e
simna <simna@labglo.com>
2020-12-16 10:55:14 +0530

    Initial Commit

---
+++ analyse/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+from django.contrib.gis.db import models
+
+class GeoUSA(models.Model):
+    name = models.CharField(max_length=25) # corresponds to the 'str' field
+    poly = models.PolygonField(srid=4269) # we want our model in a different SRID
+
+    def __str__(self):
+        return 'Name: %s' % self.name
