7addd952127dd986fb30e4b1e1d20467295fbe49
simna <simna@labglo.com>
2020-12-22 17:48:23 +0530

    flood zone mapping method

---
+++ analyse/utils.py
@@ -0,0 +1,12 @@
+from analyse.models import GeoUSA
+from FloodData.custom import CustomLayerMapping
+
+
+mapping = {'name' :'FLD_ZONE', # The 'name' model field maps to the 'str' layer field.
+               'poly' : 'POLYGON', # For geometry fields use OGC name.
+               }
+
+
+def flood_zone_mapping(path, county):
+    lm = CustomLayerMapping(GeoUSA, path, mapping, custom={"county": county}
+    lm.save()
